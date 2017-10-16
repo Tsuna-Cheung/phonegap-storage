a24bbcc4a266d0eb953fc7c3d24ab19f330bc20c
Tsuna-Cheung <237971264@qq.com>
2017-10-16 10:27:56 +0100

    start

---
+++ www/js/storage.js
@@ -0,0 +1,7 @@
+window.localStorage.setItem("fruit","apple");
+widnow.localStorage.setItem("colour","red");
+window.localStorage.setItem("number","1");
+
+var value1=window.localStorage.getItem("fruit");
+var value2=window.localStorage.getItem("colour");
+window.alert(value1);
